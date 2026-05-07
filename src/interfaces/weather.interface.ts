2d4f5e84611cf04e80fa4e56fe45bf5f0fc3b604
Leonardo Caggianelli <leonardo.caggianelli@skianet.it>
2026-05-07 19:22:25 +0200

    feat: add weather interface

---
+++ src/interfaces/weather.interface.ts
@@ -0,0 +1,63 @@
+export interface OpenweatherGeolocation {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
+
+export interface OpenweatherWeather {
+  coord: {
+    lat: number;
+    lon: number;
+  };
+  weather: {
+    main: string;
+    description: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+  sys: {
+    country: string;
+  }
+  name: string;
+}
+
+// export interface OpenweatherWeather {
+//   coord: OpenweatherCoordProp;
+//   weather: OpenweatherWeatherWeatherProp[];
+// }
+
+// export interface OpenweatherCoordProp {
+//   lat: number;
+//   lon: number;
+// }
+
+// export interface OpenweatherWeatherWeatherProp {
+//   main: string;
+//   description: string;
+// }
+
+
+
+
+
+
+export interface WeatherProps {
+  city: string;
+  country: string;
+  weather_condition: string;
+  date: string;
+  weather_temperature: number;
+  weather_wind: number;
+  weather_feel_temperature: number;
+  weather_uv: number;
+  weather_pressure: number;
+}
